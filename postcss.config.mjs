79f41d80adc067b32df263faa54983c00deebfe3
Habib Ur Rehman <146686867+Mian007-Habib@users.noreply.github.com>
2025-04-20 11:48:07 +0500

    SaaS Code Editor

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,3 +3,7 @@ const config = {
 };
 
 export default config;
+
+//PostCSS is a tool that processes your CSS and can apply plugins to do things like add prefixes, minify, etc.
+// PostCSS is a tool for transforming CSS with JavaScript plugins.
+// It is a JavaScript-based CSS preprocessor that allows you to use plugins to lint your CSS, support variables and mixins, transpile future CSS syntax, inline images, and more
